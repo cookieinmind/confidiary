22e96522ba3e7e8af2f3ed46ab2bb9c1b24ece1f
Juan Ramos <jc.ramos.contact@gmail.com>
2022-01-28 16:50:16 -0400

    bug fix

--- pages/Days.tsx
+++ pages/Days.tsx
@@ -62,7 +62,6 @@ export default function Days() {
         (x) => !_todayEntries.includes(x)
       );
       const _organizedEntries = organizeEntries(notTodayEntries);
-      console.table(_organizedEntries);
       setOganizedEntries(_organizedEntries);
     }
 
